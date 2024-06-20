ba0bf083b0fdec8c94db47f4fe4f4530041e8980
Abhishek Kapoor <ab210637@dal.ca>
2024-06-20 07:47:34 -0300

    Add problem statement to the 189

--- 189-Rotate-Array/solution.py
+++ 189-Rotate-Array/solution.py
@@ -1,3 +1,7 @@
+'''
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+'''
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
