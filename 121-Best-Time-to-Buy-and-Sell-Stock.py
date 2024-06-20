67561cee1e697694604abf94205574764e65989d
Abhishek Kapoor <ab210637@dal.ca>
2024-06-20 20:11:36 -0300

    Add 189, 169, 121

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -0,0 +1,22 @@
+'''
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+'''
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        m = float('inf')
+        mm = 0
+        
+        for p in prices:
+            if(p<m):
+                m = p
+            elif(p - m > mm):
+                mm = p-m
+        
+        return mm
+        
+
+        
---
+++ 169-Majority-Element.py
@@ -0,0 +1,18 @@
+'''
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+'''
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        for num in nums:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+        l = len(nums)
+        for num in nums:
+            if count[num] > l/2:
+                return num
